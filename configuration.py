31ca679136e72463899ee65d79aa65618433f3da
istojan <stoleilieski@gmail.com>
2020-10-06 10:35:01 +0200

    Create a configuration file

--- configuration.py
+++ configuration.py
@@ -1,2 +1,16 @@
+settings = {
+    "male_names_file_path": "other/male_names.txt",
+    "female_names_file_path": "other/female_names.txt",
+    "output_file_path": "output/language_model_large_v4.pickle",
+    "dictionary_file_path": "other/macedonian-dict",
+    "language_model_service_address": "http://127.0.0.1:5000",
+    "validation_system_files_directory": "../other/validation_system/example2/",
+    "validation_system_input_file_name": "example_text.txt",
+}
 
-LANGUAGE_MODEL_FILE_PATH = "models/language_model_books2_600_v1.pickle"
+
+LANGUAGE_MODEL_FILE_PATH = "models/big_test_trimmed_first-level-10_other-levels-2.pickle"
+
+NAME_JOKER_SIGN = "#######"
+
+NUMBER_JOKER_SIGN = "$$$$$$$"
